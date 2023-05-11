9587d39842680a84f2fbfa500a3ac1717ccc2a16
SyedMFahad <smfahad750@gmail.com>
2023-05-11 21:38:25 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,5 @@
+public class Node {
+    void run(){
+        System.out.println("from node");
+    }
+}
